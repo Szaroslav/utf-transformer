4e3a21c3046c8e4a00cc2948e3af5a8b6ab0286c
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-09-18 21:18:24 +0200

    Init transform function

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const stringLiterals = {
+  "'": function () {
+
+  },
+  "\"": function () {
+
+  },
+  "`": function () {
+
+  }
+};
+
+export function transform(text) {
+  let i = 0;
+  while (i < text.length) {
+    i++;
+  }
+}
