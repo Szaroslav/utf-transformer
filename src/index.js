79c29a32cc1b05a906dd7c396652a0a47c531c2d
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-09-19 21:13:22 +0200

    Create basic string detection (only for single and double quotes)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 const stringLiterals = {
-  "'": function () {
-
+  "\"": function (text, start) {
+    let i = start + 1;
+    while (i < text.length) {
+      if (text[Math.max(i - 1, 0)] === "\\") {
+        i += 2;
+        continue;
+      }
+      if (text[i] === "\"") {
+        return [ start, i ];
+      }
+      i++;
+    }
   },
-  "\"": function () {
-
+  "'": function (text, start) {
+    let i = start + 1;
+    while (i < text.length) {
+      if (text[Math.max(i - 1, 0)] === "\\") {
+        i += 2;
+        continue;
+      }
+      if (text[i] === "'") {
+        return [ start, i ];
+      }
+      i++;
+    }
   },
-  "`": function () {
+  "`": function (text, start) {
 
   }
 };
 
 export function transform(text) {
+  const stringLiteralChars = Object.keys(stringLiterals);
+
   let i = 0;
   while (i < text.length) {
+    if (text[Math.max(i - 1, 0)] === "\\") {
+      i += 2;
+      continue;
+    }
+    for (const stringLiteralChar of stringLiteralChars) {
+      if (text[i] === stringLiteralChar) {
+        console.log(stringLiterals[stringLiteralChar]);
+        const [ start, end ] = stringLiterals[stringLiteralChar](text, i);
+        console.log(stringLiteralChar, start, end, text.slice(start, end + 1));
+        i = end;
+        break;
+      }
+    }
     i++;
   }
 }
